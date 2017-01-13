1d15fdd5bd440b5fffedf266afde80c3388311b4
Ethan Bellora <Belloramail@gmail.com>
2017-01-13 11:22:41 -0600

    prep repo for heroku

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
+//use node port or default to 3000
+const PORT = process.env.PORT || 3000;
+
+//direct all https to http
+app.use(function(req,res,next){
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(err){
+
+
+app.listen(PORT, function(err){
 	if (err) {
 		console.log(err);
 	} else {
-		console.log('app is up and running');
+		console.log('app is up and running on port: ' + PORT);
 	}
 });
